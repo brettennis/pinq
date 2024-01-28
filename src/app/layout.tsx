7283272e6e169ee0597e3998729528b26bfb6d9c
Brett Ennis <brettennis4@gmail.com>
2024-01-28 16:37:25 -0500

    added theme constants to global css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,17 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, DM_Serif_Display } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ 
+  variable: '--font-inter',
+  subsets: ["latin"] 
+});
+
+const dm = DM_Serif_Display({
+  variable: '--font-dm',
+  subsets: ["latin"],
+  weight: '400',
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +24,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${inter.variable} ${dm.variable}`}>
+      <body>{children}</body>
     </html>
   );
 }
